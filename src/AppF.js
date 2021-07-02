4202455a68337bb96c0c2ecb46ea49cecfd15673
ramhead68 <86788915+ramhead68@users.noreply.github.com>
2021-07-02 19:04:56 +0700

    Initial commit

---
+++ src/AppF.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './AppF.css';
+import Button from '@material-ui/core/Button';
+
+function AppF() {
+  return (
+  <div className="AppF" class='centered'>
+  <h1>New learn Thank you!!
+    <p><Button variant="contained" color="primary">Hello React</Button></p>
+
+</h1>
+ </div>
+
+ );
+}
+
+export default AppF;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import AppF from './AppF';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <AppF />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+reportWebVitals();
+
